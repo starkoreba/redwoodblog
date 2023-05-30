b37e43cb6a5c53c3555872151772c8b2a2138260
Maeva De Campos <kulwilcoox@gmail.com>
2023-05-30 08:09:07 +0200

    Generate type for scaffold posts

--- web/src/components/ArticlesCell/ArticlesCell.tsx
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -6,6 +6,9 @@ export const QUERY = gql`
   query ArticlesQuery {
     articles: posts {
       id
+      title
+      body
+      createdAt
     }
   }
 `
@@ -21,9 +24,15 @@ export const Failure = ({ error }: CellFailureProps) => (
 export const Success = ({ articles }: CellSuccessProps<ArticlesQuery>) => {
   return (
     <ul>
-      {articles.map((item) => {
-        return <li key={item.id}>{JSON.stringify(item)}</li>
-      })}
+      {articles.map((article) => (
+        <article key={article.id}>
+          <header>
+            <h2>{article.title}</h2>
+          </header>
+          <p>{article.body}</p>
+          <div>Posted at: {article.createdAt}</div>
+        </article>
+      ))}
     </ul>
   )
 }
